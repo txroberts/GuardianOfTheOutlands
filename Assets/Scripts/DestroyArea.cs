7c9afd019b91b04103ca017aa3faab1ac824929a
txroberts <t.x.roberts@dundee.ac.uk>
2015-03-27 00:39:16 +0000

    Destroy Area collider
    Changed to destroy any object exiting the destroy area, except the
    player (which wraps around to the other side of the screen)

--- Assets/Scripts/DestroyArea.cs
+++ Assets/Scripts/DestroyArea.cs
@@ -5,26 +5,22 @@ public class DestroyArea : MonoBehaviour {
 	void OnTriggerExit2D (Collider2D c){
 		string layerName = LayerMask.LayerToName (c.gameObject.layer); // Get the layer name
 
-		if (layerName.Equals ("Bullet (Player)")) {
+		// Destory anything that isn't the player and return straight away
+		if (!layerName.Equals ("Player"))
 			Destroy (c.gameObject);
-		} else if (layerName.Equals ("Barrel")) {
-			Enemy enemy = c.transform.parent.GetComponent<Enemy> ();
-			enemy.currentState = "No Target";
-			Destroy (c.gameObject);
-		} else if (layerName.Equals ("Player")) {
-			Transform player = c.transform;
-			Vector3 playerPosition = Camera.main.WorldToViewportPoint (player.position);
-			Vector3 newPosition = player.position;
 
-			if (playerPosition.x > 1 || playerPosition.x < 0)
-				newPosition.x = -newPosition.x;
+		// The player 'screen wraps'
+		Transform player = c.transform;
+		Vector3 playerPosition = Camera.main.WorldToViewportPoint (player.position);
+		Vector3 newPosition = player.position;
+		
+		if (playerPosition.x > 1 || playerPosition.x < 0)
+			newPosition.x = -newPosition.x;
+		
+		if (playerPosition.y > 1 || playerPosition.y < 0)
+			newPosition.y = -newPosition.y;
+		
+		player.position = newPosition;
 
-			if (playerPosition.y > 1 || playerPosition.y < 0)
-				newPosition.y = -newPosition.y;
-
-			player.position = newPosition;
-		} else {
-			Destroy (c.gameObject);
-		}
 	}
 }
