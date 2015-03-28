284335abf13c7f628a430dce82bad55eec30a805
txroberts <t.x.roberts@dundee.ac.uk>
2015-03-28 21:37:45 +0000

    Added score GUI + script
    Added the score GUI to the screen and the script used to update the text
    it displays

---
+++ Assets/Scripts/GUI/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+	Text scoreGUI;
+	int score;
+
+	void Start () {
+		scoreGUI = GetComponent<Text> ();
+		score = 0;
+	}
+
+	void FixedUpdate () {
+		scoreGUI.text = score.ToString ();
+	}
+
+	public void addPoints (int points) {
+		score += points;
+	}
+}
