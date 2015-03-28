8ec67c9175c77648a6ca4931b6268eeeb9b12a36
txroberts <t.x.roberts@dundee.ac.uk>
2015-03-28 16:10:20 +0000

    Added player bullet pool
    Added player bullet pool empty that instantiates and stores a pool of
    player bullets

---
+++ Assets/Scripts/PlayerBulletPool.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerBulletPool : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	List<GameObject> bullets;
+	public int bulletPoolSize = 15;
+
+	void Start () {
+		bullets = new List<GameObject> ();
+		for (int i = 0; i < bulletPoolSize; i++) {
+			GameObject bullet = (GameObject) Instantiate(bulletPrefab);
+			bullet.SetActive(false);
+			bullet.transform.parent = transform;
+			bullets.Add(bullet);
+		}
+	}
+}
