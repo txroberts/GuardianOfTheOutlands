388508429d9157cebf6c4ab0095bf3ffbfc1f1fc
txroberts <t.x.roberts@dundee.ac.uk>
2015-03-30 00:22:51 +0100

    Click button to play game
    Added that clicking the 'Play Game' button switches to the game play
    scene. Added Instructions menu screen.

---
+++ Assets/Scripts/Menu/MenuNavigation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour {
+	
+	public void switchToScene (string newScene) {
+		Application.LoadLevel (newScene);
+	}
+}
