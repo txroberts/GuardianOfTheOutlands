9aae5aa44df2976809c9b69bff56941216bc3c54
txroberts <t.x.roberts@dundee.ac.uk>
2015-04-06 21:10:05 +0100

    Vehicle Factory - Enemy
    Vehicle Factory makes enemies - Game Manager calls it repeatedly to make
    waves of random enemies

--- Assets/Scripts/Gameplay/Factories/VehicleFactory.cs
+++ Assets/Scripts/Gameplay/Factories/VehicleFactory.cs
@@ -8,7 +8,11 @@ public class VehicleFactory : MonoBehaviour {
 	public GameObject createVehicle (string type) {
 		GameObject vehicle = null;
 
-		if (type.Equals ("player")) {
+		if (type.Equals("enemy")) {
+			// create a random type of enemy
+			int randomType = Random.Range(0, enemyTypes.Length);
+			vehicle = (GameObject) Instantiate (enemyTypes [randomType]);
+		} else if (type.Equals ("player")) {
 			// create player
 			vehicle = (GameObject) Instantiate (playerPrefab);
 		}
--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -2,31 +2,96 @@
 
 public class GameManager : MonoBehaviour {
 
-	public int numberOfPlayers = 1;
+	VehicleFactory vehicleFactory;
+
 	public int numberOfEnemies = 5;
-	public int numberOfBarrels = 5;
 
 	public GameObject enemies, barrels, players;
 
+	void Start () {
+		vehicleFactory = GetComponent<VehicleFactory> ();
+
+		spawnPlayer();
+		spawnWave(); // spawn first wave of enemies
+	}
+
 	void Update () {
-		if (players.transform.childCount < 1){
-			// Spawn a player at a random position, with a random rotation
-			GameObject player = GetComponent<VehicleFactory>().createVehicle("player");
+		if (enemies.transform.childCount == 0 && numberOfEnemies > 0) { // all enemies in a wave have been destroyed
 
-			if (player != null){
-				player.transform.parent = players.transform;
+			// Add points to the player's score for each remaining barrel
+			int remainingBarrels = barrels.transform.childCount;
+			if (remainingBarrels > 0){
+				int barrelPointsValue = barrels.GetComponentInChildren<Barrel>().pointsValue;
+				FindObjectOfType<Score>().addPoints(remainingBarrels * barrelPointsValue);
 
-				float randX = Random.Range (0f, 0.9f); // random point on the x-axis
-				float randY = Random.Range (0f, 0.9f); // random point on the y-axis
-				Vector3 spawnPosition = Camera.main.ViewportToWorldPoint (new Vector3 (randX, randY, 10));
-
-				player.transform.position = spawnPosition;
-				player.transform.rotation = Quaternion.Euler (0, 0, Random.Range (0f, 360f));
-
-				// ** Test for spawning another co-op player **
-				//Vector3 testSpawn = player.transform.position + transform.forward * 2.0f;
-				//Instantiate (player, testSpawn, Quaternion.identity);
+				spawnWave(); // spawn the next wave of enemies
+			} else {
+				// end game screen
 			}
 		}
+
+		if (players.transform.childCount == 0){
+			spawnPlayer();
+		}
+	}
+
+	void spawnPlayer ()	{
+		GameObject player = vehicleFactory.createVehicle("player"); // vehicle factory creates a player
+
+		player.transform.position = randomScreenPosition ();
+		player.transform.rotation = Quaternion.Euler (0, 0, Random.Range (0f, 360f));
+		player.transform.parent = players.transform;
+		
+		// ** Test for spawning another co-op player **
+		//Vector3 testSpawn = player.transform.position + transform.forward * 2.0f;
+		//Instantiate (player, testSpawn, Quaternion.identity);
+	}
+	
+	Vector3 randomScreenPosition ()	{
+		float randX = Random.Range (0f, 0.9f); // random point on the x-axis
+		float randY = Random.Range (0f, 0.9f); // random point on the y-axis
+		return Camera.main.ViewportToWorldPoint (new Vector3 (randX, randY, 10));
+	}
+
+	void spawnWave () {
+		for (int i = 0; i < numberOfEnemies; i++) {
+			GameObject enemy = vehicleFactory.createVehicle("enemy");
+			enemy.transform.position = randomScreenEdgePosition();
+			enemy.transform.rotation = Quaternion.identity;
+			enemy.transform.parent = enemies.transform;
+		}
+	}
+	
+	Vector3 randomScreenEdgePosition () {
+		Vector3 position;
+		float randX, randY;
+
+		// choose an edge to spawn on (0 = top, 1 = right, 2 = bottom, 3 = left)
+		int randEdge = Random.Range(0, 4);
+		
+		// Random spawn position on the edge of the screen
+		switch (randEdge){
+			case 0: // top edge
+				randX = Random.Range(0f, 1f); // random point on the x-axis
+				position = Camera.main.ViewportToWorldPoint(new Vector3(randX,1,10));
+				break;
+			case 1: // right edge
+				randY = Random.Range(0f, 1f); // random point on the y-axis
+				position = Camera.main.ViewportToWorldPoint(new Vector3(1,randY,10));
+				break;
+			case 2: // bottom edge
+				randX = Random.Range(0f, 1f); // random point on the x-axis
+				position = Camera.main.ViewportToWorldPoint(new Vector3(randX,0,10));
+				break;
+			case 3: // left edge
+				randY = Random.Range(0f, 1f); // random point on the y-axis
+				position = Camera.main.ViewportToWorldPoint(new Vector3(0,randY,10));
+				break;
+			default: // default bottom-left corner
+				position = Camera.main.ViewportToWorldPoint(new Vector3(0,0,10));
+				break;
+		}
+
+		return position;
 	}
 }
